8fd151e4032e1df0a93e3dd7cea846c4b79c3dd4
vungoctan1109 <vntan1109@gmail.com>
2021-05-12 14:29:29 +0700

    CRUD category

--- app.js
+++ app.js
@@ -3,20 +3,37 @@ const app = express();
 const port = 3000;
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const session = require('express-session');
+const sessionStore = new session.MemoryStore;
+const { flash } = require('express-flash-message');
 const db = mongoose.connection;
+const categoryRoute = require('./routes/category_route')
 app.set("view engine", "ejs");
 app.set("views", "views");
 app.use(express.static("public"));
+
+//flash message
+app.use(
+    session({
+      secret: 'secret',
+      resave: false,
+      saveUninitialized: true,
+      cookie: {
+        maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
+        // secure: true, // becareful set this option, check here: https://www.npmjs.com/package/express-session#cookiesecure. In local, if you set this to true, you won't receive flash as you are using `http` in local, but http is not secure
+      },
+    })
+);
+app.use(flash({ sessionKeyName: 'flashMessage' }));
+
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+app.use('/admin/categories', categoryRoute);
 
 //mongoose
 // login connection into mongodb
 mongoose.connect(
-  "mongodb+srv://admin:admin@cluster2.6wtsh.mongodb.net/Callygraphy?retryWrites=true&w=majority",
+  "mongodb+srv://admin:admin@cluster0.znxdx.mongodb.net/TH2012E_Project_Sem1_Final?retryWrites=true&w=majority",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -25,34 +42,11 @@ mongoose.connect(
 // notified connection mongodb!
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", function () {
-  
 });
-
-const Cat = mongoose.model("Cat", { name: String });
-console.log("Connection!");
-
-
-
-
-
 
 
 ////////////////////////////////
-app.get("/demo/mongooes", (req,res) => {
-  const kitty = new Cat({ name: req.query.name });
-  kitty.save().then(() => console.log(kitty.name));
-  res.send("create success!");
-});
 
-//admin-create
-app.get('/admin/product/form/create', (req, res) => {
-  res.render('admin/product/form.ejs');
-  
-})
-//create
-app.post("/admin/product/form/create", (req, res) => {
-  res.send(req.body);
-});
 //home
 app.get('/user/page/home',(req, res) => {
   res.render('user/page/home.ejs');
@@ -68,4 +62,12 @@ app.get("/user/page/gallery", (req, res) => {
 // about
 app.get("/user/page/about", (req, res) => {
   res.render("user/page/about_us.ejs");
+});
+//blog_detail
+app.get("/user/page/blog_detail", (req, res) => {
+  res.render("user/page/blog_detail.ejs");
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
 });
---
+++ controllers/category_controller.js
@@ -0,0 +1,58 @@
+const Category = require('../models/category');
+
+exports.getList = function (req, resp){
+    Category.find().then(async function (data){
+        resp.render('admin/category/list.ejs', {
+            list: data,// dữ liệu từ db
+            message: await req.consumeFlash('message')
+        });
+    });
+}
+
+exports.edit = function (req, resp){
+    Category.findById(req.query.id).then(function (data){
+        resp.render('admin/category/edit.ejs', {
+            item: data
+        });
+    });
+}
+
+exports.update = function (req, resp){
+    Category.findByIdAndUpdate(req.query.id, req.body).then(function (data){
+        resp.redirect('/admin/categories');
+    });
+}
+
+exports.delete = function (req, resp){
+    Category.findById(req.query.id).then(function (data){
+        resp.render('admin/category/delete.ejs', {
+            item: data
+        });
+    });
+}
+
+exports.doDelete = function (req, resp){
+    Category.findByIdAndDelete(req.query.id).then(function (data){
+        resp.redirect('/admin/categories');
+    });
+}
+
+exports.create = function (req, resp){
+    resp.render('admin/category/form.ejs');
+}
+
+exports.store = function (req, resp){
+    const obj = new Category(req.body);
+    const error = obj.validateSync();
+    if(error && error.errors){
+        resp.render('admin/category/form.ejs', {
+            item: obj,
+            errors: error.errors
+        });
+    }else {
+        obj.save().then(async function () {
+            await req.flash('message', 'Create success.');
+            resp.redirect('/admin/categories');
+        })
+    }
+}
