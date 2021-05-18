2ba7fd5fbda18d7814bf2ca935696b6d41805b5a
vungoctan1109 <vntan1109@gmail.com>
2021-05-18 22:13:59 +0700

    add user admin controller route file

--- routes/user/user-gallery-router.js
+++ routes/user/user-gallery-router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const route = express.Router();
-const usergalleryController = require("../controllers/user-gallery-controller");
+const usergalleryController = require("../../controllers/user/user-gallery-controller");
 
 route.get('/', usergalleryController.getlist_gallery);
 module.exports = route;
