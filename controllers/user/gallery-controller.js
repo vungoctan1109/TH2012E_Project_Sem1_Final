a6e36fff088486f7850c3b5ccdd86253194466fb
vungoctan1109 <vntan1109@gmail.com>
2021-05-20 15:27:34 +0700

    add pagination user

--- controllers/user/gallery-controller.js
+++ controllers/user/gallery-controller.js
@@ -1,10 +1,21 @@
 const Gallery = require('../../models/gallery.js');
+require('mongoose-pagination');
 
 exports.getlist_gallery = function (req,res) {
-    Gallery.find().then( async function (data) {
+    var page = parseInt(req.query.page);
+    var limit = 9;
+    Gallery.find()
+        .paginate(
+            page,
+            limit,
+            async function (err, data, totalItem) {
         //render view kèm theo dữ liệu
         res.render('user/page/gallery', {
-            list: data
+            list: data,
+            totalItem: totalItem,
+            totalPage: Math.ceil(totalItem/limit),
+            page: page,
+            limit: limit
         });
     });
 }
