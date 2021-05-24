5486c01512c474ddcc6dd13d24114086857e7b2e
Nguyen Van Binh <Nguyenvanbinh31012001@gmail.com>
2021-05-24 10:08:08 +0700

    update validate sigup/in; reponsive article-detail

--- public/js/signUp-login.js
+++ public/js/signUp-login.js
@@ -17,4 +17,39 @@ $(window).on("load", function (event) {
 });
 
 
- 
+// $(".btn-login").onclick = function () {
+//   alert("1")
+//   if ($(".userName").value.length == 0) {
+//     $(".text-danger").innerText = "Nhap user name";
+//   }
+// };
+
+
+var btnLogin = document.querySelector(".btn-login");
+var btnSignUp = document.querySelector(".btn-signUp");
+var textDanger = document.querySelector(".text-danger");
+var userName = document.querySelector(".userName");
+var userPassword = document.querySelector(".userPassword");
+var signUpUserName = document.querySelector(".signUp-userName");
+var signUpEmail = document.querySelector(".signUp-email");
+var signUpPassword = document.querySelector(".signUp-password")
+btnLogin.onclick = function () {
+   if (userName.value.length == 0) {
+     alert("Please enter Username!");
+   }
+   if (userPassword.value.length == 0) {
+     alert("Please enter Password!");
+   }
+}
+
+btnSignUp.onclick = function () {
+  if (signUpUserName.value.length == 0) {
+    alert("Please enter Username!");
+  }
+  if (signUpEmail.value.length == 0) {
+    alert("Please enter email!");
+  }
+  if (signUpPassword.value.length == 0) {
+    alert("Please enter Password!");
+  }
+}
